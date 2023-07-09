c7a87ba1d7fca8e5d420f36000e41e007aad6902
marainev <mrvgllms@gmail.com>
2023-07-09 15:03:14 -0400

    data

---
+++ src/data/Data.js
@@ -0,0 +1,165 @@
+const Data = [
+    {
+      id: 1001,
+      title: "1984",
+      author: "George Orwell",
+      description: "A dystopian novel depicting a totalitarian regime.",
+      price: 9.99,
+      imageURL: "https://example.com/1984.jpg"
+    },
+    {
+      id: 1002,
+      title: "The Hitchhiker's Guide to the Galaxy",
+      author: "Douglas Adams",
+      description: "A comedic science fiction series exploring the absurdity of the universe.",
+      price: 14.99,
+      imageURL: "https://example.com/hitchhikers-guide.jpg"
+    },
+    {
+      id: 1003,
+      title: "Neuromancer",
+      author: "William Gibson",
+      description: "A cyberpunk novel about a washed-up computer hacker.",
+      price: 11.99,
+      imageURL: "https://example.com/neuromancer.jpg"
+    },
+    {
+      id: 1004,
+      title: "Solaris",
+      author: "Stanislaw Lem",
+      description: "A philosophical novel set on a sentient oceanic planet.",
+      price: 10.99,
+      imageURL: "https://example.com/solaris.jpg"
+    },
+    {
+      id: 1005,
+      title: "The Time Machine",
+      author: "H.G. Wells",
+      description: "A classic tale of time travel and its consequences.",
+      price: 8.99,
+      imageURL: "https://example.com/time-machine.jpg"
+    },
+    {
+      id: 1006,
+      title: "Metropolis",
+      author: "Thea von Harbou",
+      description: "A futuristic novel set in a city ruled by machines.",
+      price: 12.99,
+      imageURL: "https://example.com/metropolis.jpg"
+    },
+    {
+      id: 1007,
+      title: "Brave New World",
+      author: "Aldous Huxley",
+      description: "A dystopian novel where society is genetically engineered.",
+      price: 13.99,
+      imageURL: "https://example.com/brave-new-world.jpg"
+    },
+    {
+      id: 1008,
+      title: "The War of the Worlds",
+      author: "H.G. Wells",
+      description: "A thrilling account of an alien invasion in Victorian England.",
+      price: 9.99,
+      imageURL: "https://example.com/war-of-the-worlds.jpg"
+    },
+    {
+      id: 1009,
+      title: "Snow Crash",
+      author: "Neal Stephenson",
+      description: "A fast-paced cyberpunk novel set in a future where virtual reality rules.",
+      price: 11.99,
+      imageURL: "https://example.com/snow-crash.jpg"
+    },
+    {
+      id: 1010,
+      title: "The Left Hand of Darkness",
+      author: "Ursula K. Le Guin",
+      description: "A groundbreaking exploration of gender and sexuality on an alien planet.",
+      price: 10.99,
+      imageURL: "https://example.com/left-hand-of-darkness.jpg"
+    },
+    {
+      id: 1011,
+      title: "Stranger in a Strange Land",
+      author: "Robert A. Heinlein",
+      description: "A story of a human raised on Mars and his return to Earth.",
+      price: 12.99,
+      imageURL: "https://example.com/stranger-in-a-strange-land.jpg"
+    },
+    {
+      id: 1012,
+      title: "The Sirens of Titan",
+      author: "Kurt Vonnegut",
+      description: "A satirical science fiction novel exploring free will and destiny.",
+      price: 9.99,
+      imageURL: "https://example.com/sirens-of-titan.jpg"
+    },
+    {
+      id: 1013,
+      title: "Do Androids Dream of Electric Sheep?",
+      author: "Philip K. Dick",
+      description: "A thought-provoking novel questioning the nature of humanity.",
+      price: 11.99,
+      imageURL: "https://example.com/do-androids-dream.jpg"
+    },
+    {
+      id: 1014,
+      title: "The Dispossessed",
+      author: "Ursula K. Le Guin",
+      description: "A tale of two planets with contrasting political and social systems.",
+      price: 10.99,
+      imageURL: "https://example.com/the-dispossessed.jpg"
+    },
+    {
+      id: 1015,
+      title: "Frankenstein",
+      author: "Mary Shelley",
+      description: "A Gothic science fiction novel about a scientist and his monstrous creation.",
+      price: 8.99,
+      imageURL: "https://example.com/frankenstein.jpg"
+    },
+    {
+      id: 1016,
+      title: "Hyperion",
+      author: "Dan Simmons",
+      description: "An epic space opera featuring a group of pilgrims and their intertwining stories.",
+      price: 12.99,
+      imageURL: "https://example.com/hyperion.jpg"
+    },
+    {
+      id: 1017,
+      title: "The Wind-Up Bird Chronicle",
+      author: "Haruki Murakami",
+      description: "A surreal novel exploring themes of loss and self-discovery.",
+      price: 11.99,
+      imageURL: "https://example.com/wind-up-bird-chronicle.jpg"
+    },
+    {
+      id: 1018,
+      title: "The Road",
+      author: "Cormac McCarthy",
+      description: "A post-apocalyptic tale of survival and the bond between a father and son.",
+      price: 9.99,
+      imageURL: "https://example.com/the-road.jpg"
+    },
+    {
+      id: 1019,
+      title: "The City and the Stars",
+      author: "Arthur C. Clarke",
+      description: "A novel set in a future city and its secrets spanning millions of years...",
+      price: 11.99,
+      imageURL: "https://example.com/city-and-the-stars.jpg"
+    },
+    {
+        id: 1022,
+        title: "Battle Royale",
+        author: "Koushun Takami",
+        description: "A dystopian novel featuring a brutal fight for survival.",
+        price: 10.99,
+        imageURL: "https://example.com/battle-royale.jpg"
+    },
+];
+
+export default Data;
+  
