5e9d35b4b59fbb43d4e18d480fe55ad0a1d80180
marainev <mrvgllms@gmail.com>
2023-07-10 14:33:00 -0400

    created two grids for herobanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,28 +5,38 @@ import '../styles/components.scss';
 
 const HeroBanner = () => {
   return (
-    <div className='hero-banner-container'>
-      <div className='eye-icon'>
-        <Eye size={190} />
-      </div>
-      &nbsp;
-      <div>  
-        <p>DELVE INTO</p>      
-        <h2>George Orwell's</h2>
-        <h1>1984</h1>
-        <h3><i>"Big Brother is watching you"</i></h3>
-      </div>
+    <>
+      <div className='grid-container'>
+        <div className='hero-banner-container welcome-container'>
+            <div>
+                <h2>Welcome to sputnik!</h2>
+                <p>Explore our collection of books and discover new worlds.</p>
+                <Link to='/books'>
+                    <button className='button' type='button'>Browse Books</button>
+                </Link>
+            </div>`
+        </div>
 
-      <div>
-        <Link to='/books/1001'>
-          <button type='button'>Shop Now</button>
-        </Link>
-        <div className='desc'>
-          <p>A dystopian novel depicting a totalitarian regime.</p>
+        <div className='hero-banner-container orwell-container'>
+            <div className='eye-icon'>
+                <Eye size={200} />
+            </div>
+            <div className='orwell-content'>
+                <p>DELVE INTO</p>
+                <h2>George Orwell's</h2>
+                <h1>1984</h1>
+                <h3>
+                    <i>"Freedom is the freedom to say that two plus two make four."</i>
+                </h3>
+                <Link to='/books/1001'>
+                    <button className='button' type='button'>Shop Now</button>
+                </Link>
+            </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default HeroBanner;
+
