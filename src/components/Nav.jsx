a179674633d4f33ab19876a13cb75fc690e7d8b2
marainev <mrvgllms@gmail.com>
2023-07-10 12:04:03 -0400

    updated styles for Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,53 +1,56 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ShoppingBag, Circle, List } from 'phosphor-react';
+import { ShoppingBag, Circle, List, MagnifyingGlass } from 'phosphor-react';
 
 const Nav = ({ cartItems }) => {
-  const [menuVisible, setMenuVisible] = useState(false);
+    const [menuVisible, setMenuVisible] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
 
-  const getCartItemCount = () => {
-    return cartItems.reduce((count, item) => count + item.quantity, 0);
-  };
+    const getCartItemCount = () => {
+        return cartItems.reduce((count, item) => count + item.quantity, 0);
+    };
 
-  return (
-    <div className="navbar-container">
-      <div>
-        <button className="button-add-to-cart" onClick={toggleMenu}>
-          <List size={50} />
-        </button>
-        {menuVisible && (
-           <div>
-           <p className="logo">
-             <Link to="/">Home</Link>
-           </p>
-           <p className="logo">
-             <Link to="/books">Books</Link>
-           </p>
-           <p className="logo">
-             <Link to="/about">About</Link>
-           </p>
-           <p className="logo">
-             <Link to="/contact">Contact</Link> {/* New "Contact" link */}
-           </p>
-         </div>
-        )}
-      </div>
-      <div className="icon-container">
-        <Link to="/cart" className="button-add-to-cart">
-          <ShoppingBag size={50} />
-          {cartItems.length > 0 && (
-            <span className="cart-item-count">
-              <Circle size={12} color="#FBBF24" weight="bold" /> {getCartItemCount()}
-            </span>
-          )}
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className='navbar-container'>
+            <div>
+                <button className='phosphor-icon' onClick={toggleMenu}>
+                <List size={50} />
+                </button>
+                {menuVisible && (
+                <div>
+                <p className='logo'>
+                    <Link to='/'>Home</Link>
+                </p>
+                <p className='logo'>
+                    <Link to='/books'>Books</Link>
+                </p>
+                <p className='logo'>
+                    <Link to='/about'>About</Link>
+                </p>
+                <p className='logo'>
+                    <Link to='/contact'>Contact</Link>
+                </p>
+                </div>
+                )}
+            </div>
+            <div className='icon-container'>
+                <Link to='/search' className='phosphor-icon'>
+                <MagnifyingGlass size={45} />
+                </Link>
+                <Link to='/cart' className='phosphor-icon'>
+                <ShoppingBag size={50} />
+                {cartItems.length > 0 && (
+                    <span className='cart-item-count'>
+                    <Circle size={12} color='#FBBF24' weight='bold' /> {getCartItemCount()}
+                    </span>
+                )}
+                </Link>
+            </div>
+        </div>
+    )
 };
 
 export default Nav;
