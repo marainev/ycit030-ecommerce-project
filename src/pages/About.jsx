891e3978080ea472af2346668fbaa1f4540445f1
marainev <mrvgllms@gmail.com>
2023-07-10 14:33:52 -0400

    changed the about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,20 +7,20 @@ const About = () => {
             <h2>About</h2>
             <div className='about'>
                 <p>
-                    Sea Gato is an independent bookstore for sci-fi enthusiasts in the heart of Prague. Since its establishment, we have been 
+                    sputnik is an independent bookstore for sci-fi enthusiasts in the heart of Prague. Since its establishment, we have been 
                     diligently curating a collection of science fiction books that will transport you to otherworldly realms and ignite your 
                     imagination. Our store is a sanctuary for avid readers seeking thrilling adventures, mind-bending concepts, and thought-provoking 
                     narratives.
                 </p> 
                 &nbsp;
                 <p>
-                    At Sea Gato, we take pride in offering a carefully selected range of science fiction literature, including classic works, 
+                    At sputnik, we take pride in offering a carefully selected range of science fiction literature, including classic works, 
                     contemporary masterpieces, and hidden gems waiting to be discovered. From dystopian futures to interstellar journeys, our shelves are filled with 
                     captivating stories that explore the wonders and possibilities of the cosmos.
                 </p>  
                 &nbsp;
                 <p> 
-                    Whether you are a seasoned sci-fi aficionado or just dipping your toes into the genre, Sea Gato invites you to embark on an extraordinary reading journey. 
+                    Whether you are a seasoned sci-fi aficionado or just dipping your toes into the genre, sputnik invites you to embark on an extraordinary reading journey. 
                 </p>  
                 &nbsp;   
                 <p>
@@ -29,7 +29,7 @@ const About = () => {
             </div>
             <Footer/>
         </div>
-    )
+    );
 };
 
 export default About;
